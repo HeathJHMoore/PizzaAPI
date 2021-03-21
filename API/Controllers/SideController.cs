dee98afa093dcdb46ba7cb1aaecc626f8bdbc889
Itai-Kumengisa <imehku222@gmail.com>
2021-03-21 02:03:43 -0400

    get sides/pizza/drinks from database

---
+++ API/Controllers/SideController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace API.Controllers
+{
+    public class SideController : ApiController
+    {
+        // GET api/<controller>
+        public HttpResponseMessage Get()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["PizzaAppDB"].ConnectionString;
+
+            SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+            string query = "SELECT * From dbo.Side";
+
+            SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+
+            DataTable dataTable = new DataTable();
+
+            using (sqlDataAdapter)
+            {
+                sqlCommand.CommandType = CommandType.Text;
+                sqlDataAdapter.Fill(dataTable);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, dataTable);
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
